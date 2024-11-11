6e2eb43e069ade984569b95e4d55a4fc6cb1fa4a
Thor Johan Berntsen Kjelsrud <157597601+thokje16@users.noreply.github.com>
2024-11-11 12:57:33 +0100

    start

---
+++ vm/variables.tf
@@ -0,0 +1,37 @@
+variable "vm_name" {
+  type        = string
+  description = "Navnet på VM-en"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure-regionen"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Navnet på ressursgruppen"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID-en til subnettet VM-en skal være i"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "Størrelsen på VM-en"
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Brukernavn for VM-administratoren"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Brukernavn for VM-administratoren"
+  default     = "Passord123!"
+}
+
