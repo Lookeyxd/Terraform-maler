10a69b03aea205a525a430ab285063935769491e
Thor Johan Berntsen Kjelsrud <157597601+thokje16@users.noreply.github.com>
2024-11-11 13:21:30 +0100

    asdasd

--- vm/main.tf
+++ vm/main.tf
@@ -1,25 +1,71 @@
-resource "azurerm_network_interface" "main" {
-  name                = "${var.vm_name}-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+terraform {
+  required_version = ">=0.12"
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
   }
 }
 
-resource "azurerm_linux_virtual_machine" "main" {
-  name                = var.vm_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  network_interface_ids = [azurerm_network_interface.main.id]
-  disable_password_authentication = false
+provider "azurerm" {
+  features {}
+}
 
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.address_space]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
+
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+  size = var.vm_size
 
   os_disk {
     caching              = "ReadWrite"
@@ -27,9 +73,14 @@ resource "azurerm_linux_virtual_machine" "main" {
   }
 
   source_image_reference {
-    publisher = "Debian"
-    offer     = "debian-12"
-    sku       = "12"
-    version   = "latest"
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
   }
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  disable_password_authentication = false
 }
--- vm/variables.tf
+++ vm/variables.tf
@@ -1,37 +1,93 @@
-variable "vm_name" {
+variable "resource_group_name" {
+  description = "Navn på ressursgruppen"
   type        = string
-  description = "Navnet på VM-en"
 }
 
 variable "location" {
+  description = "Plassering for ressursene i Azure"
   type        = string
-  description = "Azure-regionen"
+  default     = "Norway East"
 }
 
-variable "resource_group_name" {
+variable "vnet_name" {
+  description = "Navn på virtual network"
   type        = string
-  description = "Navnet på ressursgruppen"
+  default     = "myVNet"
 }
 
-variable "subnet_id" {
+variable "address_space" {
+  description = "Adresseområde for virtual network"
   type        = string
-  description = "ID-en til subnettet VM-en skal være i"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_name" {
+  description = "Navn på subnett"
+  type        = string
+  default     = "mySubnet"
+}
+
+variable "subnet_address_prefix" {
+  description = "Adresseområde for subnett"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_ip_name" {
+  description = "Navn på public IP"
+  type        = string
+  default     = "myPublicIP"
+}
+
+variable "nic_name" {
+  description = "Navn på network interface"
+  type        = string
+  default     = "myNIC"
+}
+
+variable "vm_name" {
+  description = "Navn på virtuell maskin"
+  type        = string
+  default     = "myVM"
 }
 
 variable "vm_size" {
+  description = "Størrelse på virtuell maskin"
   type        = string
-  description = "Størrelsen på VM-en"
-  default     = "Standard_B1s"
+  default     = "Standard_DS1_v2"
 }
 
 variable "admin_username" {
+  description = "Administratorbrukernavn for VM"
   type        = string
-  description = "Brukernavn for VM-administratoren"
 }
 
 variable "admin_password" {
+  description = "Administratorpassord for VM"
   type        = string
-  description = "Brukernavn for VM-administratoren"
-  default     = "Passord123!"
+  sensitive   = true
 }
 
+variable "image_publisher" {
+  description = "Bildeleverandør for VM"
+  type        = string
+  default     = "Debian"
+}
+
+variable "image_offer" {
+  description = "Image offer for VM"
+  type        = string
+  default     = "debian-12"
+}
+
+variable "image_sku" {
+  description = "Image SKU for VM"
+  type        = string
+  default     = "12"
+}
+
+variable "image_version" {
+  description = "Bildeversjon for VM"
+  type        = string
+  default     = "latest"
+}
